58eabcf6d2d9421e0102879909dcf6e2e17fc9da
Weixian Zhang <weixzha@microsoft.com>
2019-05-16 10:53:57 +0800

    initial project structure of Project Elenktis

---
+++ Informer/MandateServiceInformer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace elenktis.Informer.MandateServiceInformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
