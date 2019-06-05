2b3ba97d2b1d29175517f3beb1b13a28185a7acc
Weixian Zhang <weixzha@microsoft.com>
2019-06-05 23:44:02 +0800

    updated Docker.dev, func cli is installed and function is able to run. Pending to solve ssh remote debug

---
+++ Deployment/azure/az-setup-elenktis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#get aad tenant id
+
+#create service principal
+	#get client id
+	#get client secret
+
+#create cosmos mongo api
+	# get connectstring
+
+#create az service bus
+	# get connectstring
+
+#create akv
+	#create secrets for above values
+
+#create AKS
