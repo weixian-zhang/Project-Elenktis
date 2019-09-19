46ac58ffe1a6a78309ffd478bfd359b1036f05f6
19hours <kevinchuwinghim@gmail.com>
2019-09-19 14:33:58 +0800

    added subscriptions api controller

---
+++ Web/Elenktis.Web.Api/Controllers/SubscriptionsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+// using Elenktis.Policy;
+// using Elenktis.Policy.DefaultService;
+// using Elenktis.PlanQueryManager;
+
+namespace Elenktis.Web.Api.Subscriptions
+{
+    [ApiController]
+    [Route("api/subscriptions")]
+    public class SubscriptionsController : ControllerBase
+    {
+        [HttpGet]
+        public String GetValue()
+        {
+            // await _policyStore.GetPolicyAsync<ASCAutoRegisterVMEnabledPolicy>("21214214214");
+            return "test";
+        }
+
+        // private PlanQueryManager _planQueryManager;
+    }
+}
